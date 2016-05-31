fd881e49fb355c9c813888923994d8b427606fff
Qiang Zhou <chinaboy@udel.edu>
2016-05-31 11:54:08 -0400

    change byte reader

--- trade.cpp
+++ trade.cpp
@@ -16,8 +16,10 @@ class OrderFillMessage;
 
 class Trader{
 public:
-	Trader(uint8_t firm_id, string trader_tag, uint32_t qty): firm_id(firm_id), trader_tag(trader_tag), qty(qty){
-		
+	Trader(uint8_t firm_id, string trader_tag, uint32_t qty){
+		this->firm_id = firm_id;
+		this->trader_tag = trader_tag;
+		this->qty = qty;
 	}
 	uint8_t firm_id;
 	string trader_tag; // char [3]
@@ -26,10 +28,22 @@ public:
 
 class BytesReader{
 public:
-	BytesReader(uint8_t* buffer) {
+	BytesReader(string stream){
+		this->f.open(stream.c_str(), ifstream::binary);
 		termination.assign("BDBDBDBD");
-		this->buffer = buffer;
 		this->pos = 0;
+	}
+
+	~BytesReader(){
+		delete[] this->buffer;
+	}
+
+	void setBufferSize(int fix_size){
+		if(this->buffer){
+			delete[] this->buffer;
+		}
+		this->buffer = new uint8_t[n];
+		this->f.read(buffer, fix_size);
 	}
 
 	uint8_t getUint8(){
@@ -45,9 +59,8 @@ public:
 		return result;
 	}
 
-	uint32_t getUint32(){
-		int pos = this->pos;
-		uint8_t* buffer = this->buffer + pos;
+	uint32_t getUint32(){	
+		uint8_t* buffer = this->buffer + this->pos;
 		uint32_t result = ((uint32_t)buffer[3] << 24) | ((uint32_t)buffer[2] << 16) | ((uint32_t)buffer[1] << 8) | buffer[0];
 		this->pos += 4;
 		return result;
@@ -55,7 +68,7 @@ public:
 
 	uint64_t getUint64(){
 		int pos = this->pos;
-		uint8_t* buffer = this->buffer + pos;
+		uint8_t* buffer = this->buffer + this->pos;
 		uint64_t result = ((uint64_t)buffer[7] << 56) | ((uint64_t)buffer[6] << 48) | ((uint64_t)buffer[5] << 40) |((uint64_t)buffer[4] << 32 ) |
 					((uint64_t)buffer[3] << 24) | ((uint64_t)buffer[2] << 16) | ((uint64_t)buffer[1] << 8) | buffer[0];
 		this->pos += 8;
@@ -66,8 +79,58 @@ public:
 		string s( (const char*)this->buffer, n);
 		this->pos += n;
 		return s;
+	}	
+
+private:
+	ifstream f;
+	uint8_t* buffer;
+	int pos;
+	string termination;
+};
+
+class Header{
+public:
+	Header(string filename) : header_size(22){	
+		this->f = f;	
+		this->br = new BytesReader(filename);		
 	}
 
+	~Header(){
+		delete this->br;
+	}
+
+	void read(){
+		this->br->setBufferSize(header_size);
+		 
+		this->marker = this->br->getUint16();		 
+		this->msg_type = this->br->getUint8();			
+		this->sequence_id = this->br->getUint64(); 
+		this->timestamp = this->br->getUint64();
+		this->msg_direction = this->br->getUint8();
+		this->msg_len = this->br->getUint16();
+		op();
+	}
+
+	void op(){
+			switch(this->msg_type){
+				case 1:
+					OrderEntryMessage oem(this);
+
+					break;
+				case 2:
+					OrderAckMessage oam(this);
+					break;
+				case 3;
+					OrderFillMessage ofm(this);
+					break;
+				default:
+					throw runtime_error("unknown message type");
+			}
+	}
+
+	bool next(){
+		return ! this->f.eof();
+	}
 
 	string getMaxChars(ifstream &f){
 		string s;
@@ -94,59 +157,11 @@ public:
 		return std::move(v);
 	}
 
-private:
-	uint8_t* buffer;
-	int pos;
-	string termination;
-};
-
-class Header{
-public:
-	Header(ifstream &f) : header_size(22){	
-		this->f = f;	
-		this->buffer = new uint8_t[header_size];		
-	}
-
-	~Header(){
-		delete[] buffer;
-	}
-
-	void read(){
-		this->f.read(buffer, header_size);
-		BytesReader br(buffer);
-		this->marker = br.getUint16();		 
-		this->msg_type = br.getUint8();			
-		this->sequence_id = br.getUint64(); 
-		this->timestamp = br.getUint64();
-		this->msg_direction = br.getUint8();
-		this->msg_len = br.getUint16();
-		op();
-	}
-
-	void op(){
-			switch(this->msg_type){
-				case 1:
-					OrderEntryMessage oem(this, f);
-					break;
-				case 2:
-					OrderAckMessage oam(this, f);
-					break;
-				case 3;
-					OrderFillMessage ofm(this, f);
-					break;
-				default:
-					throw runtime_error("unknown message type");
-			}
-	}
-
-	bool next(){
-		return ! this->f.eof();
-	}
-
+	BytesReader * getBytesReader(){return br;}
 private:
 	ifstream f;
 	int header_size;
-	uint8_t *buffer;
+	BytesReader *br;
 	uint16_t marker;
 	uint8_t msg_type;
 	uint64_t sequence_id;
@@ -154,33 +169,30 @@ private:
 	uint8_t msg_direction;
 	uint16_t msg_len;
 };
-/*
+
 class OrderEntryMessage{
 public:
-	OrderEntryMessage(Header * hdr, ifstream &f): fix_size(37){   // exclude variable firm string and termination; max 255 and 8
+	OrderEntryMessage(Header * hdr): fix_size(37){   // exclude variable firm string and termination; max 255 and 8
 		//this.hdr = std::move(hdr);
 		this->hdr = hdr;
-		uint8_t *buffer = new uint8_t[fix_size];
-		f.read(buffer, fix_size);
-
-		BytesReader br(buffer);
+		BytesReader br = hdr->getBytesReader();
+		br->setBufferSize(fix_size);
 		
-		this->price = br.getUint64();
-		this->qty = br.getUint32();
+		this->price = br->getUint64();
+		this->qty = br->getUint32();
 		// copy 10 chars to instrument
-		this->instrument = br.getChars(10);
+		this->instrument = br->getChars(10);
 
-		this->getUint8 = br.getUint8();
-		this->client_assigned_id = br.getUint64();
-		this->time_in_force = br.getUint8();
+		this->getUint8 = br->getUint8();
+		this->client_assigned_id = br->getUint64();
+		this->time_in_force = br->getUint8();
 
 		// copy 3 chars to trader tag
-		this->trader_tag = br.getChars(3);
-		this->firm_id = br.getUint8();
+		this->trader_tag = br->getChars(3);
+		this->firm_id = br->getUint8();
 
 		// read until termination string
-		this->firm = br.getMaxChars();
-		delete[] buffer;
+		this->firm = br->getMaxChars();
 	}
 
 private:
@@ -202,16 +214,13 @@ class OrderAckMessage{
 public:
 	OrderAckMessage(Header * hdr, ifstream &f) : fix_size(14){
 		this->hdr = hdr;
-		uint8_t *buffer = new uint8_t[fix_size];
-		f.read((char*)buffer, fix_size);
-
-		BytesReader br(buffer);
+		BytesReader br = hdr->getBytesReader();
+		br->setBufferSize(fix_size);
 		
-		this->order_id = br.getUint32();
-		this->client_id = br.getUint64();
-		this->order_status = br.getUint8();
-		this->reject_code = br.getUint8();
-		delete[] buffer;
+		this->order_id = br->getUint32();
+		this->client_id = br->getUint64();
+		this->order_status = br->getUint8();
+		this->reject_code = br->getUint8();
 	}
 
 private:
@@ -228,18 +237,14 @@ class OrderFillMessage{
 public:
 	OrderFillMessage(Header * hdr, ifstream &f) : fix_size(17){
 		this->hdr = hdr;
-		this->hdr = hdr;
-		uint8_t* buffer = new uint8_t[fix_size];
-		f.read((char*)buffer, fix_size);
+		BytesReader br = hdr->getBytesReader();
+		br->setBufferSize(fix_size);
 
-		BytesReader br(buffer);
-		this->order_id = br.getUint32();
-		this->fill_price = br.getUint64();
-		this->fill_qty = br.getUint32();
-		this->no_of_contras = br.getUint8();
-		this->traders = br.getTraders(f);
-		
-		delete[] buffer;
+		this->order_id = br->getUint32();
+		this->fill_price = br->getUint64();
+		this->fill_qty = br->getUint32();
+		this->no_of_contras = br->getUint8();
+		this->traders = br->getTraders(f);
 	}
 
 	~OrderFillMessage(){
@@ -254,12 +259,12 @@ private:
 	vector<Trader> traders;
 	int fix_size;
 };
-*/
+
 int parseStream(string stream){
-	ifstream input(stream.c_str(), ifstream::binary);
+	
 
 	
-	Header hdr(input);
+	Header hdr(stream);
 	while( hdr.next() ){
 		hdr.read();
 		break;
